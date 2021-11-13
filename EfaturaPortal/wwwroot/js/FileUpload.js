f68d8a7f75a82c35966db044c68550c3ed49e6c6
Ali Zeki KILIÇ <ali@goktas.net>
2021-11-13 23:27:13 +0300

    fc

---
+++ EfaturaPortal/wwwroot/js/FileUpload.js
@@ -0,0 +1,17 @@
+﻿function GetFileBinary(file) {
+
+    var reader = new FileReader();
+
+    var arrayBuffer = reader.readAsArrayBuffer(file);
+    
+    array = new Uint8Array(arrayBuffer);
+
+    binaryString = String.fromCharCode.apply(null, array);
+
+
+    console.log(binaryString);
+
+
+
+
+}
