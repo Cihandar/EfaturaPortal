d8e19795f0a3045cbda8dc55230e767719a84556
ThinkPad <ThinkPad@AZK>
2022-01-11 17:23:36 +0300

    ccc

--- EfaturaPortal/Application/EfaturaApi/Command/CreateUbl.cs
+++ EfaturaPortal/Application/EfaturaApi/Command/CreateUbl.cs
@@ -50,7 +50,11 @@ namespace EfaturaPortal.Application.EfaturaApi.Command
             invoice.DocumentCurrencyCode = new DocumentCurrencyCodeType { Value = faturaVM.Doviz };
             invoice.LineCountNumeric = new LineCountNumericType { Value = 1 };
 
-            invoice.AdditionalDocumentReference = await GetDocumentReference(faturaVM);
+            invoice.AdditionalDocumentReference = await GetDocumentReference(faturaVM); //Fatura Dizaynı Basılıyor...
+
+            invoice.Signature = await GetSignatureTypes(faturaVM);  //E-imza / Mali Mühür için Gönderen Firma Bilgileri basılıyor...
+
+            //invoice.AccountingSupplierParty = new SupplierPartyType
 
 
 
@@ -108,25 +112,28 @@ namespace EfaturaPortal.Application.EfaturaApi.Command
         {
             var docref = new List<DocumentReferenceType>();
 
-            docref.Add(
-                new DocumentReferenceType
-                {
-                    ID = new IDType { Value = Guid.NewGuid().ToString() },
-                    IssueDate = new IssueDateType { Value = faturaVM.Tarih },
-                    DocumentType = new DocumentTypeType { Value = "XSLT" },
-                    Attachment = new AttachmentType
-                    {
-                        EmbeddedDocumentBinaryObject = new EmbeddedDocumentBinaryObjectType
-                        {
-                            characterSetCode = "UTF-8",
-                            encodingCode = "Base64",
-                            filename = "default.xslt",
-                            mimeCode = "application/xml",
-                            Value = await GetXSLTFiletoBinary(faturaVM.SeriNumaralar.SablonDosyaAdi)
-                        }
-                    }
-                });
 
+            if (!string.IsNullOrWhiteSpace(faturaVM.SeriNumaralar.SablonDosyaAdi) && File.Exists("/uploads/Xslt/" + faturaVM.SeriNumaralar.SablonDosyaAdi))
+            {
+                docref.Add(
+                    new DocumentReferenceType
+                    {
+                        ID = new IDType { Value = Guid.NewGuid().ToString() },
+                        IssueDate = new IssueDateType { Value = faturaVM.Tarih },
+                        DocumentType = new DocumentTypeType { Value = "XSLT" },
+                        Attachment = new AttachmentType
+                        {
+                            EmbeddedDocumentBinaryObject = new EmbeddedDocumentBinaryObjectType
+                            {
+                                characterSetCode = "UTF-8",
+                                encodingCode = "Base64",
+                                filename = "default.xslt",
+                                mimeCode = "application/xml",
+                                Value = await GetXSLTFiletoBinary(faturaVM.SeriNumaralar.SablonDosyaAdi)
+                            }
+                        }
+                    });
+            }
             docref.Add(
                 new DocumentReferenceType
                 {
@@ -143,7 +150,6 @@ namespace EfaturaPortal.Application.EfaturaApi.Command
         private async Task<byte[]> GetXSLTFiletoBinary(string fileName)
         {
             var data = Encoding.UTF8.GetBytes(new StreamReader(new FileStream("/uploads/Xslt/" + fileName, FileMode.Open, FileAccess.Read), Encoding.UTF8).ReadToEnd());
-
             return data;
 
         }
@@ -155,10 +161,11 @@ namespace EfaturaPortal.Application.EfaturaApi.Command
             string sirketturu = "VKN";
             if (faturaVM.Firmalar.VergiNumarasi.Length == 11) sirketturu = "TCKN";
 
-            sign.Add(new SignatureType { 
-            
-                ID = new IDType { schemeID="VKN_TCKN",Value=faturaVM.Firmalar.VergiNumarasi },
-                SignatoryParty = new PartyType { }
+            sign.Add(new SignatureType
+            {
+
+                ID = new IDType { schemeID = "VKN_TCKN", Value = faturaVM.Firmalar.VergiNumarasi },
+                SignatoryParty = await GetSenderPartyType(faturaVM, sirketturu)
 
             });
 
@@ -166,7 +173,67 @@ namespace EfaturaPortal.Application.EfaturaApi.Command
             return sign.ToArray();
         }
 
-       
+        private async Task<PartyType> GetSenderPartyType(FaturaGetAllQueryViewModel faturaVM, string sirketturu)
+        {
+          var party =  new PartyType
+            {
+
+                PartyIdentification = new[]
+                {
+                    new PartyIdentificationType
+                    {
+                        ID= new IDType { schemeID = sirketturu,Value =faturaVM.Firmalar.VergiNumarasi }
+                    //},
+                    //new PartyIdentificationType
+                    //{
+                    //    ID = new IDType { schemeID = "MERSISNO", Value =  faturaVM.Firmalar.MersisNo }
+                    //},
+                    //new PartyIdentificationType
+                    //{
+                    //    ID = new IDType { schemeID = "TICARETSICILNO", Value =  faturaVM.Firmalar.TicariSicilNo }
+                    //}
+
+                }
+
+            },
+                PostalAddress = new AddressType
+                {
+                    CitySubdivisionName = new CitySubdivisionNameType { Value = faturaVM.Firmalar.Ilce },
+                    CityName = new CityNameType { Value = faturaVM.Firmalar.Sehir },
+                    Country = new CountryType { Name = new NameType1 { Value = "Türkiye" } },
+                    StreetName = new StreetNameType { Value = faturaVM.Firmalar.Adres },
+                    BuildingName = new BuildingNameType { Value = "" },
+                    PostalZone = new PostalZoneType { Value = "" },
+                    Room = new RoomType { Value = "" },
+                    Region = new RegionType { Value = "" },
+                    BlockName = new BlockNameType { Value = "" }
+                },
+                PartyName = new PartyNameType { Name = new NameType1 { Value = faturaVM.Firmalar.Unvani } },
+                //  WebsiteURI = new WebsiteURIType {  Value=faturaVM.Firmalar.WebSitesi},
+                Contact = new ContactType { ElectronicMail = new ElectronicMailType { Value = faturaVM.Firmalar.Email }, Telephone = new TelephoneType { Value = faturaVM.Firmalar.Telefon } },
+                PartyTaxScheme = new PartyTaxSchemeType { TaxScheme = new TaxSchemeType { Name = new NameType1 { Value = faturaVM.Firmalar.VergiNumarasi } } }
+
+
+
+
+            };
+
+            if (sirketturu == "TCKN")
+            {
+                var adlar = faturaVM.Firmalar.Unvani.Split(' ');
+                if (adlar.Length == 2) party.Person = new PersonType { FirstName = new FirstNameType { Value = adlar[0] }, FamilyName = new FamilyNameType { Value = adlar[1] } };
+                else if (adlar.Length == 3) party.Person = new PersonType { FirstName = new FirstNameType { Value = adlar[0] }, MiddleName = new MiddleNameType { Value = adlar[1] }, FamilyName = new FamilyNameType { Value = adlar[2] } };
+                else if (adlar.Length == 4) party.Person = new PersonType { FirstName = new FirstNameType { Value = adlar[0] }, MiddleName = new MiddleNameType { Value = adlar[1] + " " + adlar[2] }, FamilyName = new FamilyNameType { Value = adlar[3] } };
+                else { }
+            }
+
+            return party;
+
+        }
+
+        //private async Task<SupplierPartyType> GetSupplierParty
+
+
 
     }
 }
